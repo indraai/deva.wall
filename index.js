4b7ef8a06fa37af0ddd2402d126bd6dc32920878
Quinn Michaels <quinn@indra.ai>
2025-10-04 17:39:32 -0700

    wall upate deva core

--- index.js
+++ index.js
@@ -40,56 +40,23 @@ const WALL = new Deva({
     parse(input) {return input.trim();},
     process(input) {return input.trim();},
   },
-  listeners: {
-    'devacore:question'(packet) {
-      const echo = this.methods.echo('wall', 'q', packet);
-    },
-    'devacore:answer'(packet) {
-      const echo = this.methods.echo('wall', 'a', packet);
-    }    
-  },
+  listeners: {},
   modules: {},
   devas: {},
-  func: {
-    echostr(opts) {
-      const {id, agent, client, md5, sha256, sha512} = opts;
-      const created = Date.now();
-
-      this.action('func', `echostr:${id}`);
-      this.state('set', `echostr:${id}`);
-      const echostr = [
-        `::begin:wall:${id}`,
-        `transport: ${id}`, 
-        `client: ${client.profile.id}`, 
-        `agent: ${agent.profile.id}`, 
-        `created: ${created}`, 
-        `md5: ${md5}`, 
-        `sha256:${sha256}`, 
-        `sha512:${sha512}`,
-        `::end:wall:${id}`,
-      ].join('\n');
-
-      // stub for later features right now just echo into the system process for SIGINT monitoring.
-      const echo = spawn('echo', [echostr])
-      echo.stderr.on('data', err => {
-        this.error(err, opts);
-      });
-      
-      this.state('return', `echostr:${id}`);
-      return echostr;
-    }
-  },
+  func: {},
   methods: {},
   onInit(data, resolve) {
     const {personal} = this.license(); // get the license config
     const agent_license = this.info().VLA; // get agent license
     const license_check = this.license_check(personal, agent_license); // check license
     // return this.start if license_check passes otherwise stop.
+    this.action('return', `onInit:${data.id.uid}`);
     return license_check ? this.start(data, resolve) : this.stop(data, resolve);
   },
   onReady(data, resolve) {
     const {VLA} = this.info();
     this.prompt(`${this.vars.messages.ready} > VLA:${VLA.uid}`);
+    this.action('resolve', `onReady:${data.id.uid}`);
     return resolve(data);
   },
   onError(data, err, reject) {
